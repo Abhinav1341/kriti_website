dc2e59e97eed06d3d387e745aef07e1872efc8af
Abhinav <abhinavsinghhg@gmail.com>
2024-08-29 01:49:52 +0530

    footer upd

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,7 +6,7 @@ import img4 from "../images/consultation.svg";
 
 const Services = () => {
   return (
-    <div id="services" className="bg-gray-100 py-12">
+    <div id="services" className=" bg-transparent py-12">
       <section data-aos="zoom-in-down">
         <div className="my-4 py-4">
           <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">
